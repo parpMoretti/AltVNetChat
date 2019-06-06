7a6277fc1885a0af85e32fd8adcfd27e33a30309
Moretti <moretti@ls-v.com>
2019-06-06 20:01:55 +0100

    Inital Upload

---
+++ ChatExtension.cs
@@ -0,0 +1,17 @@
+﻿using AltV.Net;
+using AltV.Net.Elements.Entities;
+
+namespace Server.Extensions
+{
+    public class ChatExtension
+    {
+        public static void OnChatMessage(IPlayer player, object[] message)
+        {
+            if ((string)message[0] == "/")
+            {
+                //Handle Chat Commands
+                Alt.Log($"{player.Name} has typed {message}");
+            }
+        }
+    }
+}
